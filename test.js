3387c8cccc59467b5d9746a5316bd2de1abe80e8
festivities <admin@festivity.moe>
2026-09-02 15:06:55 +0800

    cleanup

--- test.js
+++
@@ -1,8 +0,0 @@
-
-(async () => {
-  const links = await import('./src/links.js');
-  console.log('Fetching...');
-  const res = await links.fetchGuarded('https://pbs.twimg.com/media/HKlAs4aXcAAzPut.jpg:large', 50 * 1024 * 1024);
-  console.log('Result:', res ? res.contentType : 'null');
-})();
-
--- test_filename.js
+++
@@ -1,8 +0,0 @@
-
-const path = require('path');
-const originalUrl = 'https://pbs.twimg.com/media/HKlAs4aXcAAzPut.jpg:large';
-let fileName = path.basename(new URL(originalUrl).pathname) || 'restored_media';
-fileName = fileName.split(':')[0];
-fileName = fileName.replace(/[^a-zA-Z0-9.\-_]/g, '_');
-console.log(fileName);
-
